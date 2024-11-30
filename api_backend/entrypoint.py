003cfebc80f983a78c80bcd9901c374c56507bb2
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2024-11-30 18:13:12 -0500

    update entrypoint app - add config name

--- api_backend/entrypoint.py
+++ api_backend/entrypoint.py
@@ -2,11 +2,20 @@
 app/entrypoint.py
 Punto de entrada para ejecutar aplicación
 """
+import os
+from dotenv import load_dotenv
 from app import create_app
-from app.config.settings import Setting
+from app.config.settings import ApplicationConfig
+load_dotenv()
 
-app = create_app()
-app.config.from_object(Setting)
+config_name = os.getenv('APP_ENVIRONMENT', 'development')
+
+app = create_app(config_name)
+app.config.from_object(ApplicationConfig)
 
 if __name__ == '__main__':
-    app.run(debug=Setting.DEBUG, host=Setting.HOST, port=Setting.PORT)
+    app.run(
+        debug=ApplicationConfig.DEBUG,
+        host=ApplicationConfig.HOST, 
+        port=ApplicationConfig.PORT
+    )
